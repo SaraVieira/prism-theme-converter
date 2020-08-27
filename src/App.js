2dab4731497e1c7b73c2ba700d7a12b8607c4e9e
Sara Vieira <hey@iamsaravieira.com>
2020-08-28 00:36:20 +0200

    fix build

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useState, useRef, useEffect } from "react";
+import React, { useState, useRef } from "react";
 import { makeTheme } from "./make-theme";
 import decomment from "decomment";
 import Code from "./Code";
